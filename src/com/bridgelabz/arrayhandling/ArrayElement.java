ba49f73be0ae862fee34a93c3c15aeb769279dad
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2023-01-06 23:23:40 +0530

    Added: to print the elements of an array

---
+++ src/com/bridgelabz/arrayhandling/ArrayElement.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.arrayhandling;
+
+public class ArrayElement {
+
+	public static void main(String[] args) {
+
+		// initialize array
+		int[] array = new int[] { 1, 2, 3, 4, 5 };
+		System.out.println("Element of given array: ");
+
+		// loop through the array by incrementing value of i
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+
+		}
+
+	}
+
+}
